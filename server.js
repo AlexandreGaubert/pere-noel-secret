ee0d950f38929ebcea99a59eb112e364cb3542ae
Alexandre Gaubert <alexandre2.gaubert@gmail.com>
2018-12-20 19:32:52 +0100

    first push

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/UnePouleSurUnMur', function(req, res) {
 })
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
  
 http.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
