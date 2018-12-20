8082837e9655aff7df8b2e0c69d5db31c8af2d7f
Alexandre Gaubert <alexandre2.gaubert@gmail.com>
2018-12-20 18:25:43 +0100

    lol

--- server.js
+++ server.js
@@ -84,7 +84,9 @@ app.get('/UnePouleSurUnMur', function(req, res) {
   })
 })
 
-http.listen(port, function() {
-    // console.log(`server is listening on port ${http.address()}:${http.address().port}.`)
-    console.log(http.address());
-})
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
