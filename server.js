0ac943ae0679b198da3c6b5d3f8798419d30078b
Alexandre Gaubert <alexandre2.gaubert@gmail.com>
2018-12-20 17:48:25 +0100

    change server port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express')
 var http = require('http').Server(app);
 const path = require('path')
 
-const port = 80;
+const port = 8080;
 
 app.use( express.static( `${__dirname}/build` ) );
 
